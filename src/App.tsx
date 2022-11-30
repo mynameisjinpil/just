d236f738073ede2948950c683bb09a2ddec669c4
pillillilli <skh5806@gmail.com>
2022-11-30 22:07:58 +0900

    modified:   .gitignore 	deleted:    package-lock.json 	deleted:    package.json 	deleted:    public/favicon.ico 	deleted:    public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	deleted:    src/App.test.tsx 	deleted:    src/App.tsx 	deleted:    src/data/api/videoApi.ts 	deleted:    src/data/redux/store.ts 	deleted:    src/index.css 	deleted:    src/index.tsx 	deleted:    src/react-app-env.d.ts 	deleted:    src/reportWebVitals.ts 	deleted:    src/res/logo.svg 	deleted:    src/setupTests.ts 	deleted:    src/ui/comp/display/hero/Hero.styles.tsx 	deleted:    src/ui/comp/display/hero/Hero.tsx 	deleted:    src/ui/comp/display/movie/Movies.styles.tsx 	deleted:    src/ui/comp/display/movie/Movies.tsx 	deleted:    src/ui/comp/display/navi/Header.styles.tsx 	deleted:    src/ui/comp/display/navi/Header.tsx 	deleted:    src/ui/page/MainPage.tsx 	deleted:    src/util/hooks.ts 	deleted:    tailwind.config.js 	deleted:    tsconfig.json

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import MainPage from './ui/page/MainPage';
-
-function App() {
-  return <MainPage />;
-}
-
-export default App;
--- src/util/hooks.ts
+++
@@ -1,8 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux';
-import type { TypedUseSelectorHook } from 'react-redux';
-
-import type { RootState, AppDispatch } from '../data/redux/store';
-
-// Use throughout your data instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
